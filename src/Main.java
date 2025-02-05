c43f77ebf57e31ee1845beeb022726be883acd02
jrodriguezcode <jr698751@gmail.com>
2025-02-05 10:44:34 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// switch = statement that allows a variable to be tested for equality against a list of values
+		
+		String day = "Wednesday";
+		
+		switch(day) {
+		case "Sunday": System.out.println("It's Sunday!");
+		break;
+		case "Monday": System.out.println("It's Monday!");
+		break;
+		case "Tuesday": System.out.println("It's Tuesday!");
+		break;
+		case "Wednesday": System.out.println("It's Wednesday!");
+		break;
+		case "Thursday": System.out.println("It's Thursday!");
+		break;
+		case "Friday": System.out.println("It's Friday!");
+		break;
+		case "Saturday": System.out.println("It's Saturday!");
+		break;
+		default: System.out.println("That is not a day!");
+		}
+
+	}
+
+}
